b7633fde7fd57b40b44baaccb6465e5620137a70
Ashmita Sawant <95630121+ashmitasawant@users.noreply.github.com>
2023-11-23 12:32:58 +0530

    Update json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -13,16 +13,16 @@ def json_to_csv(json_file, csv_file):
     else:
         raise ValueError("Invalid JSON format")
 
-connection = data["connections"]["connection"][0]
-fields = connection["field"]
+    connection = data["connections"]["connection"][0]
+    fields = connection["field"]
 
-# Create a CSV file and write the tabular data
-with open("table.csv", mode="w", newline="") as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["Field ID", "Encrypted Value Set", "Uses Encryption", "Component Override", "Use Default"])
-
-    for field in fields:
-        csv_writer.writerow([field["id"], field["encryptedValueSet"], field["usesEncryption"], field["componentOverride"], field["useDefault"]])
+    # Create a CSV file and write the tabular data
+     with open("table.csv", mode="w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Field ID", "Encrypted Value Set", "Uses Encryption", "Component Override", "Use Default"])
+    
+        for field in fields:
+            csv_writer.writerow([field["id"], field["encryptedValueSet"], field["usesEncryption"], field["componentOverride"], field["useDefault"]])
 
 # Replace 'input.json' and 'output.csv' with your actual file names
 json_file_path = Path(__file__).parent / 'test/test.json'
